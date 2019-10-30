5c9df9bc6329c91518d8d3911c6ea790a9daba76
mellowjoeys <mellowjoey@gmail.com>
2019-10-30 14:34:17 -0500

    added user's chore data to user show action

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,26 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
-  def chores_for_this_week
-    chores.where( "user_chores.start_of_week = ?", last_sunday)
+  def chores_for_this_week # probably unneeded considering there is a remaining_chores method. 
+    chores_this_week = chores.where( "user_chores.start_of_week = ?", last_sunday)
+    map_chores(chores_this_week)
   end
 
   def chores_for_next_week
     chores.where("user_chores.start_of_week = ?", next_sunday)
+  end
+
+  def map_chores(chores)
+    chores.map{ |chore| {id: chore.id, name: chore.name, value: chore.value }}
+  end
+
+  def remaining_chores_this_week
+    remaining_chores = chores.where( "user_chores.completed = ? and user_chores.start_of_week = ?", false, last_sunday)
+    map_chores(remaining_chores)
+  end
+
+  def completed_chores_this_week
+    completed_chores = chores.where( "user_chores.completed = ? and user_chores.start_of_week = ?", true, last_sunday)
+    map_chores(completed_chores)  
   end
 end
